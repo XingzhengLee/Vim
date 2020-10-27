a0c33b777cc79f0b4bcaa26a9f5b5beae90e3966
XingzhengLee <xingzheng.lee@gmail.com>
2020-10-27 21:59:49 +0800

    added content in the vim.sh file

---
+++ Vim.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+############################## move ##############################
+h j k l
+w b
+
+############################## insertion ##############################
+i a o s
+# insert at the begining of the line (insert)
+Shift+i (I)
+# insert at the end of the line (append)
+Shift+a (A)
+# insert at the top of the line (open a line)
+Shift+o (O)
+# insert after delete the entire line
+Shift+s (S)
+
+############################## deletion ##############################
+# delete a character
+x
+# delete + motion
+d
+
+############################## paste ##############################
+# paste after
+p
+# paste before
+P
+
+############################## revise ##############################
+# replace without insertion mode
+r
+# replace mode
+R
+# delete and insert (change)
+c
+
+############################## search ##############################
+# search after
+/
+# search before
+?
+# next
+n
+N
+# no highlight
+nohl
+
+############################## replace ##############################
+%s/old/new/g
+lineM,LineNs/old/new
+
+# split the screen
+split/vsplit
+
+# line number
+set number
+# cursorline
+set cursorline
+# show alternative command with tab
+set wildmenu
+# highlight after search
+set hlsearch
+# highlight while search 
+set incsearch
+# ingnore case
+set ignorecase
+# smart case
+set smartcase
+# to line
+number
+
+<operation> times*<motion>
+# begin
+b
+# end
+e
+# word (next)
+w
+# in
+i
+# around
+a
+# find
+f
+
+# beginning of the file
+gg
+# end of the file
+G
+
+# delete in word
+diw
+
+# undo
+u
+# undo for the entire line
+U
+# undo the undo
+Ctrl+r
+
+# statistics
+Ctrl+g
+
+# find the paired parentheses
+%
+
+# visual mode
+v
+
+# indent
+> >>
+# anti-indent
+< <<
+
+# execute bash
+:! command
+
+# append file content
+:r [file name]
+
+# open multiple files
+vim -o -O
+
+# select file
+Ctrl+w+w/hjkl
+
